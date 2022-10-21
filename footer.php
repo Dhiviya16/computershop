6b4b6b7cd267d02e1f0d7b5b616b237cbf8fc8ee
Dhiviya Rajan <82600789+Dhiviya16@users.noreply.github.com>
2022-10-21 15:42:19 +0800

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,38 @@
+<section class="footer">
+
+    <div class="box-container">
+
+        <div class="box">
+            <h3>Quick Links</h3>
+            <a href="home.php">Home</a>
+            <a href="about.php">About</a>
+            <a href="contact.php">Contact</a>
+            <a href="shop.php">Shop</a>
+        </div>
+
+        <div class="box">
+            <h3>Extra Links</h3>
+            <a href="login.php">Login</a>
+            <a href="register.php">Register</a>
+            <a href="orders.php">My Orders</a>
+            <a href="cart.php">My Cart</a>
+        </div>
+
+        <div class="box">
+            <h3>Contact Info</h3>
+            <p> <i class="fas fa-phone"></i> +60 124567890 </p>
+            <p> <i class="fas fa-envelope"></i> computershop@gmail.com </p>
+        </div>
+
+        <div class="box">
+            <h3>Follow Us</h3>
+            <a href="#"><i class="fab fa-facebook-f"></i>Facebook</a>
+            <a href="#"><i class="fab fa-twitter"></i>Twitter</a>
+            <a href="#"><i class="fab fa-instagram"></i>Instagram</a>
+        </div>
+
+    </div>
+
+    <div class="credit">&copy; copyright @ <?php echo date('Y'); ?> by <span>computerShop.</span> </div>
+
+</section>
